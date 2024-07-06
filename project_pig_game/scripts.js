7873ad1d789e2e79e736823f65139b477d01b807
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2024-07-05 22:41:18 -0700

    pig game final

--- project_pig_game/scripts.js
+++ project_pig_game/scripts.js
@@ -13,16 +13,30 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const btnReset = document.querySelector('.btn--new');
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
 
-const scores = [0, 0]; // hold player 0 & player 1
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+
+// Starting conditions
+const init = function () {
+    scores = [0, 0]; // hold player 0 & player 1
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
+
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -67,7 +81,7 @@ btnHold.addEventListener("click", function () {
             scores[activePlayer];
 
         // 2 check if play's score is >=100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             // finish the game
             playing = false;
             diceEl.classList.add("hidden");
@@ -85,16 +99,4 @@ btnHold.addEventListener("click", function () {
     }
 });
 
-btnReset.addEventListener("click", function () {
-    currentScore = 0;
-    document.getElementById("current--0").textContent = currentScore;
-    document.getElementById("current--1").textContent = currentScore;
-
-
-
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    console.log(currentScore);
-    diceEl.classList.add("hidden");
-
-});
+btnNew.addEventListener("click", init);
